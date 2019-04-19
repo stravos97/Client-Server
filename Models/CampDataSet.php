4dc2e627eda3f09480b0d582bff65f50294f211a
stravos97 <haashimalvi@protonmail.com>
2019-04-19 17:23:34 +0100

    Fixed Campsite charset issue

--- Models/CampDataSet.php
+++ Models/CampDataSet.php
@@ -44,6 +44,33 @@ class CampDataSet {
         return $dataSet;
     }
 
+    public function fetchAll() {
+
+        $sqlQuery = "SELECT *, longitude_camp, latitude_camp
+                     FROM Camp_records
+                     LEFT JOIN Camp_details ON Camp_records.id_Camps = Camp_details.id_Camps"; //second number is how many camps are shown
+
+        //$table = "Camp_records";
+
+        $statement = $this->_dbHandle->prepare($sqlQuery); // prepare a PDO statement
+
+        $statement->execute();
+        if ($statement) {
+          echo "It worked";
+        }else {
+          echo "statement -fetchall in Camp Set Failed";
+        }
+
+
+        $dataSet = [];
+      //  echo $sqlQuery;
+        while ($row = $statement->fetch()) { //executes the statement, fetches all the data and puts each sql function in the campdata constructor using row
+           $dataSet[] = new CampData($row);
+        }
+
+        return $dataSet;
+    }
+
     /**
     * Just get the Country Name which is used for autosuggest
     **/
